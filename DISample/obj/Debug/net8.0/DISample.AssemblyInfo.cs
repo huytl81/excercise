5d77097f986c9b01e721bf95288dcfc566cd4cc4
Huy Ta <huyta@viettotal.com>
2024-03-29 07:26:33 +0700

    DI

--- DISample/obj/Debug/net8.0/DISample.AssemblyInfo.cs
+++ DISample/obj/Debug/net8.0/DISample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DISample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1bf036269c0631e7b51fa9fe73472d9818ee55b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b1bc36e741a9dd07a9e2bf0161342a44070cce")]
 [assembly: System.Reflection.AssemblyProductAttribute("DISample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DISample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
