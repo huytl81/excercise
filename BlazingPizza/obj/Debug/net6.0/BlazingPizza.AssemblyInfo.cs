226602a44f4904b0a6c526a21d5141a8be588676
Huy Ta <huyta@viettotal.com>
2024-04-28 23:03:48 +0700

    BlazingPizza

--- BlazingPizza/obj/Debug/net6.0/BlazingPizza.AssemblyInfo.cs
+++ BlazingPizza/obj/Debug/net6.0/BlazingPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be605b66e117c831593eedf086bf9389f47929c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca1d2d5c49094cbf564b3616df4230d0d31dd01")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
