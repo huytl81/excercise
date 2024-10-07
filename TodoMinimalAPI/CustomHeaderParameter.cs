9b1e88fcac526bd4a5b7886df3be435648b4a676
Huy Ta <talehuy81@gmail.com>
2024-10-07 20:29:45 +0700

    x-custom-header

---
+++ TodoMinimalAPI/CustomHeaderParameter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace TodoMinimalAPI
+{
+    public class CustomHeaderParameter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            operation.Parameters =
+            [
+                new OpenApiParameter
+                {
+                    Name = "X-Custom-Header",
+                    In = ParameterLocation.Header,
+                    Description = "Custom header for authentication",
+                    Required = true
+                },
+            ];
+        }
+    }
+}
