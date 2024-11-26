9507920fd008d4f879dc27f224947a030b317c34
Huy Ta <talehuy81@gmail.com>
2024-11-26 17:50:05 +0700

    to get latest code

--- TodoControllerAPI/obj/Debug/net8.0/TodoControllerAPI.AssemblyInfo.cs
+++ TodoControllerAPI/obj/Debug/net8.0/TodoControllerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoControllerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78e70ac4c7bc6af4a96ac9012356a1a45593c8e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1e88fcac526bd4a5b7886df3be435648b4a676")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoControllerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoControllerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
