aeea508cb23b5d412452370a49a14e53d8a333ba
Ta Le Huy <talehuy81@gmail.com>
2025-10-14 09:43:37 +0700

    to get latest

--- ASP.NET/Acme.BookStore/src/Acme.BookStore.Application.Contracts/Books/IBookAppService.cs
+++ ASP.NET/Acme.BookStore/src/Acme.BookStore.Application.Contracts/Books/IBookAppService.cs
@@ -5,6 +5,6 @@ using Volo.Abp.Application.Services;
 namespace Acme.BookStore.Books;
 
 public interface IBookAppService : ICrudAppService<BookDto, Guid, PagedAndSortedResultRequestDto, CreateUpdateBookDto>
-    {
-        
-    }
+{
+    
+}
